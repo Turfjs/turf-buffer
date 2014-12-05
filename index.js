2e8d9d458045cd7fc80c0bcf65136c120928f33c
morganherlocker <morgan.herlocker@gmail.com>
2014-12-05 14:20:52 -0500

    point buffer implemented

--- index.js
+++ index.js
@@ -1,63 +1,35 @@
-// http://stackoverflow.com/questions/839899/how-do-i-calculate-a-point-on-a-circles-circumference
-// radians = degrees * (pi/180)
-// https://github.com/bjornharrtell/jsts/blob/master/examples/buffer.html
+var featurecollection = require('turf-featurecollection');
+var destination = require('turf-destination');
+var polygon = require('turf-polygon');
 
-var featurecollection = require('turf-featurecollection')
-var polygon = require('turf-polygon')
-var combine = require('turf-combine')
-var jsts = require('jsts')
+module.exports = function(feature, radius, units, resolution){
+  if(!resolution) resolution = 36;
+  var geom = feature.geometry
+  if(geom.type === 'Point') {
+    return pointBuffer(feature, radius, units, resolution);
+  } else if(geom.type === 'MultiPoint') {
 
-module.exports = function(feature, radius, units, done){
-  var buffered;
+  } else if(geom.type === 'LineString') {
 
-  done = done || function () {};
+  } else if(geom.type === 'MultiLineString') {
 
-  switch(units){
-    case 'miles':
-      radius = radius / 69.047
-      break
-    case 'kilometers':
-      radius = radius / 111.12
-      break
-    case 'degrees':
-      break
-  }
+  } else if(geom.type === 'Polygon') { 
 
-  if(feature.type === 'FeatureCollection'){
-    var multi = combine(feature);
-    multi.properties = {}
+  } else if(geom.type === 'MultiPolygon') {
 
-    buffered = bufferOp(multi, radius);
-
-    done(null, buffered);
-    return buffered;
-  }
-  else{
-    buffered = bufferOp(feature, radius);
-    
-    done(null, buffered);
-    return buffered;
   }
 }
 
-var bufferOp = function(feature, radius){
-  var reader = new jsts.io.GeoJSONReader()
-  var geom = reader.read(JSON.stringify(feature.geometry))
-  var buffered = geom.buffer(radius);
-  var parser = new jsts.io.GeoJSONParser()
-  buffered = parser.write(buffered)
-
-  if(buffered.type === 'MultiPolygon'){
-    buffered = {
-      type: 'Feature',
-      geometry: buffered,
-      properties: {}
-    }
-    buffered = featurecollection([buffered])
+function pointBuffer (pt, radius, units, resolution) {
+  var ring = []
+  var resMultiple = 360/resolution;
+  for(var i  = 0; i < resolution; i++) {
+    var spoke = destination(pt, radius, i*resMultiple, units)
+    ring.push(spoke.geometry.coordinates)
   }
-  else{
-    buffered = featurecollection([polygon(buffered.coordinates)])
+  if((ring[0][0] !== ring[ring.length-1][0]) && (ring[0][1] != ring[ring.length-1][1])) {
+    ring.push([ring[0][0], ring[0][1]]);
   }
-
-  return buffered;
+  
+  return polygon([ring])
 }
