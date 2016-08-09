Turfjs/turf-buffer

commit f75aee9dd5d70f310ee8289147a43747d71f72ae

allow FeatureCollection input

index.js
@@ -5,10 +5,24 @@ var helpers = require('turf-helpers');
 var union = require('turf-union');
 var difference = require('turf-difference');
 
-module.exports = function(feature, radius, units, resolution){
+module.exports = buffer;
+
+function buffer(feature, radius, units, resolution){
   if (!resolution) resolution = 32; // Same value as JSTS
   if (radius < 0) throw new Error("The buffer radius must be positive");
   if (radius == 0) return feature;
+  if (feature.type === 'FeatureCollection') {
+    var buffers = [];
+    feature.features.forEach(function(ft) {
+      var featureBuffer = buffer(ft, radius, units, resolution);
+      if (featureBuffer.type === 'Feature') {
+        buffers.push(featureBuffer);
+      } else { // featureBuffer.type === 'FeatureCollection'
+        buffers.push.apply(buffers,featureBuffer.features);
+      }
+    });
+    return helpers.featureCollection(buffers)
+  }
   var geom = feature.geometry;
   if (geom === null) return feature;
   if(geom.type === 'Point') {

commit a14757117da3cfcac335db8e13be0ede339cf2bf

Fix neglecting of holes caused by f75aee9dd5d70f310ee8289147a43747d71f72ae due to shallow copy

index.js
@@ -188,20 +188,19 @@ function arc(pt, radius, bearing1, bearing2, units, resolution, right, shortcut)
 }
 
 function filterNetWinding(fc, filterFn) {
+  var output = {type: 'FeatureCollection', features: []};
   var i = fc.features.length;
   while (i--) {
-    if (!filterFn(fc.features[i].properties.netWinding)) {
-        fc.features.splice(i, 1);
-    } else {
-      fc.features[i].properties = {};
+    if (filterFn(fc.features[i].properties.netWinding)) {
+        output.features.push({type: "Feature", geometry: fc.features[i].geometry, properties: {}});
     }
   }
-  return fc;
+  return output;
 }
 
 function unionFeatureCollection(fc) {
   // Note: union takes a polygon, but return a polygon or multipolygon (which it can not take in). In case of buffes, however, it will always return a polygon
-  if (fc.features.length == 0) return {type: "Feature", geometry: null};
+  if (fc.features.length == 0) return {type: "Feature", geometry: null, properties: {}};
   var incrementalUnion = fc.features[0];
   if (fc.features.length == 1) return incrementalUnion;
   for (var i = 1; i < fc.features.length; i++) {